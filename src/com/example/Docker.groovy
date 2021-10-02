6d1c1b62aea1fd4b4b027e693c69cf8b6bf871a1
SimonGPort <simon.giroux.portelance@gmail.com>
2021-10-02 15:27:44 -0400

    package groovy separated

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -7,11 +7,26 @@ class Docker implements Serializable{
     Docker(script){
         this.script=script
     }
+
+//    def buildDockerImage(String imageName){
+//        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//            script.sh "docker build -t $imageName ."
+//            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+//            script.sh "docker push $imageName"
+//        }
+//    }
+
     def buildDockerImage(String imageName){
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin(){
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t $imageName ."
             script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-            script.sh "docker push $imageName"
         }
+    }
+
+    def dockerPush(){
+        script.sh "docker push $imageName"
     }
 }
