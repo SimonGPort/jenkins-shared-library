a323e0046da1fe258e8a8f7c8ebdf745666e9284
SimonGPort <simon.giroux.portelance@gmail.com>
2021-10-02 15:07:26 -0400

    typo forget a }

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/env groovy
 
-def call(String imageName){
-    withCredentials([usernamePassword(credentialsId:'docker-hub-repo',passwordVariable:'PASS',usernameVariable:'USER')]){
+def call(String imageName) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push $imageName"
+    }
 }
